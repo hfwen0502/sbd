71709e24ade8373a5b58ae5441345b03c0c2018b
Jun Doi <doichan@jp.ibm.com>
2025-11-28 19:31:12 +0900

    fix exchange after sync

--- include/sbd/chemistry/tpb/mult_thrust.h
+++ include/sbd/chemistry/tpb/mult_thrust.h
@@ -833,30 +833,29 @@ void mult(const thrust::device_vector<ElemT> &hii,
             auto time_slid_end = std::chrono::high_resolution_clock::now();
             auto time_slid_count = std::chrono::duration_cast<std::chrono::microseconds>(time_slid_end - time_slid_start).count();
             time_slid += 1.0e-6 * time_slid_count;
-        } else {
-            // exchange T asynchronously for later tasks
-            for (size_t extask = task_sent + 1; extask < data.helper.size(); extask++) {
-                if (data.helper[extask].taskType == 0 && extask != data.helper.size() - 1) {
+        }
+        // exchange T asynchronously for later tasks
+        for (size_t extask = task_sent + 1; extask < data.helper.size(); extask++) {
+            if (data.helper[extask].taskType == 0 && extask != data.helper.size() - 1) {
 #ifdef SBD_DEBUG_MULT
-                    size_t adet_rank = mpi_rank_b / bdet_comm_size;
-                    size_t bdet_rank = mpi_rank_b % bdet_comm_size;
-                    size_t adet_rank_task = (adet_rank + data.helper[extask].adetShift) % adet_comm_size;
-                    size_t bdet_rank_task = (bdet_rank + data.helper[extask].bdetShift) % bdet_comm_size;
-                    size_t adet_rank_next = (adet_rank + data.helper[extask + 1].adetShift) % adet_comm_size;
-                    size_t bdet_rank_next = (bdet_rank + data.helper[extask + 1].bdetShift) % bdet_comm_size;
-                    std::cout << " mult: task " << task << " at mpi process (h,b,t) = ("
-                                << mpi_rank_h << "," << mpi_rank_b << "," << mpi_rank_t
-                                << "): two-dimensional slide communication from ("
-                                << adet_rank_task << "," << bdet_rank_task << ") to ("
-                                << adet_rank_next << "," << bdet_rank_next << ")"
-                                << std::endl;
+                size_t adet_rank = mpi_rank_b / bdet_comm_size;
+                size_t bdet_rank = mpi_rank_b % bdet_comm_size;
+                size_t adet_rank_task = (adet_rank + data.helper[extask].adetShift) % adet_comm_size;
+                size_t bdet_rank_task = (bdet_rank + data.helper[extask].bdetShift) % bdet_comm_size;
+                size_t adet_rank_next = (adet_rank + data.helper[extask + 1].adetShift) % adet_comm_size;
+                size_t bdet_rank_next = (bdet_rank + data.helper[extask + 1].bdetShift) % bdet_comm_size;
+                std::cout << " mult: task " << task << " at mpi process (h,b,t) = ("
+                            << mpi_rank_h << "," << mpi_rank_b << "," << mpi_rank_t
+                            << "): two-dimensional slide communication from ("
+                            << adet_rank_task << "," << bdet_rank_task << ") to ("
+                            << adet_rank_next << "," << bdet_rank_next << ")"
+                            << std::endl;
 #endif
-                    int adetslide = data.helper[extask].adetShift - data.helper[extask + 1].adetShift;
-                    int bdetslide = data.helper[extask].bdetShift - data.helper[extask + 1].bdetShift;
-                    mpi2dslider.ExchangeAsync(T[active_T], T[recv_T], adet_comm_size, bdet_comm_size, adetslide, bdetslide, b_comm, extask);
-                    task_sent = extask;
-                    break;
-                }
+                int adetslide = data.helper[extask].adetShift - data.helper[extask + 1].adetShift;
+                int bdetslide = data.helper[extask].bdetShift - data.helper[extask + 1].bdetShift;
+                mpi2dslider.ExchangeAsync(T[active_T], T[recv_T], adet_comm_size, bdet_comm_size, adetslide, bdetslide, b_comm, extask);
+                task_sent = extask;
+                break;
             }
         }
 
