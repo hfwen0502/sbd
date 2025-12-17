7b2de691d4bfad5de96cce684fb29134a598071d
t-sirakawa <t-shirakawa@riken.jp>
2025-12-17 11:23:35 +0900

    first touch for add-gdb-sbd

---
+++ include/sbd/chemistry/gdb/helper.h
@@ -0,0 +1,10 @@
+/**
+@file sbd/chemistry/gdb/helpers.h
+@brief Helper array to construct Hamiltonian for general determinant basis
+ */
+#ifndef SBD_CHEMISTRY_GDB_HELPER_H
+#define SBD_CHEMISTRY_GDB_HELPER_H
+
+
+
+#endif
---
+++ include/sbd/chemistry/gdb/inc_all.h
@@ -0,0 +1,6 @@
+#ifndef SBD_CHEMISTRY_GDB_INC_ALL_H
+#define SBD_CHEMISTRY_GDB_INC_ALL_H
+
+#include "sbd/chemistry/gdb/helper.h"
+
+#endif
